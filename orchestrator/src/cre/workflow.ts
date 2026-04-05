74eadd15c5251702fd60bf67258513f4eaeff1d9
alibenyezza <149864846+alibenyezza@users.noreply.github.com>
2026-04-05 02:49:37 +0200

    clean files

--- orchestrator/src/cre/workflow.ts
+++
@@ -1,8 +0,0 @@
-// workflow.ts - Chainlink CRE Orchestrator Entrypoint
-// export const ConfidentialHTTPClient = ... (attente lib officielle)
-
-export const startWorkflow = async () => {
-    console.log("Initializing ArcMind Orchestrator Workflow...");
-    // TODO: Setup CronTrigger
-    // TODO: Fetch from Confidential HTTP
-};
